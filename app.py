78fd702df3441fd390069c17a8911d5b6bf8dc80
Jen <je100@cs.wellesley.edu>
2024-01-16 15:31:18 -0500

    created basic app program

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import (Flask, render_template, make_response, url_for, request,
+                   redirect, flash, session, send_from_directory, jsonify)
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+# one or the other of these. Defaults to MySQL (PyMySQL)
+# change comment characters to switch to SQLite
+
+import cs304dbi as dbi
+# import cs304dbi_sqlite3 as dbi
+
+import secrets
+
+app.secret_key = 'your secret here'
+# replace that with a random key
+app.secret_key = secrets.token_hex()
+
+# This gets us better error messages for certain common request errors
+app.config['TRAP_BAD_REQUEST_ERRORS'] = True
+
+@app.route('/')
+def index():
+    return render_template('test.html',page_title='test')
+
+
+if __name__ == '__main__':
+    import sys, os
+    if len(sys.argv) > 1:
+        # arg, if any, is the desired port number
+        port = int(sys.argv[1])
+        assert(port>1024)
+    else:
+        port = os.getuid()
+    # set this local variable to 'wmdb' or your personal or team db
+    db_to_use = 'put_database_name_here_db' 
+    print('will connect to {}'.format(db_to_use))
+    dbi.conf(db_to_use)
+    app.debug = True
+    app.run('0.0.0.0',port)
